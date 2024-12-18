9db6642d5a084521bfade5586205a816a863e41f
Hardi <hardpr22@gmail.com>
2024-12-18 16:24:20 +0700

    finishing aplikasi todo

--- todo_app.php
+++ todo_app.php
@@ -1,41 +1,57 @@
 <?php
 $todos = [];
+if(file_exists('todo.txt')){
+    $file = file_get_contents('todo.txt');
+    $todos = unserialize($file);
+}
+
 if(isset($_POST['todo'])){
-    $data = $S_POST['todo'];
+    $data = $_POST['todo'];
     $todos[] = [
         'todo' => $data,
         'status' => 0
     ];
-    file_put_contents('todo.txt', serialize($todos));
+    saveData($todos);
+}   
+
+if(isset($_GET['status'])){
+    $todos[$_GET['key']]['status'] = $_GET['status'];
+    saveData($todos);
 }
+
+if(isset($_GET['hapus'])){
+    unset($todos[$_GET['key']]);
+    saveData($todos);
+}
+
+function saveData($todos){
+    file_put_contents('todo.txt', serialize($todos));
+    header('Location:todo_app.php');
+}
+
 ?>
 
 <h1>Todo App</h1>
 
 <form method="POST">
     <label for="">Apa Kegitan hari ini</label> <br>
-    <input type="text" nama="todo">
+    <input type="text" name="todo">
     <button type="submit" >Simpan</button>
 </form>
 <ul>
+    <?php foreach ($todos as $key => $value): ?>
     <li>
-        <input type="checkbox" name="todo">
-        <label for="">Todo 1</label>
-        <a href="#">Hapus</a>
+        <input type="checkbox" name="todo" onclick="window.location.href ='todo_app.php?status=<?php echo ($value['status'] == 1) ? '0' : '1'; ?>&key=<?php echo $key; ?>'" <?php if($value['status'] == 1) echo 'checked';?>>
+        <label >
+            <?php 
+                if($value['status'] == 1)
+                    echo '<del>' .$value['todo'];
+                else
+                    echo $value['todo'];
+            ?>
+        </label>
+        <a href="todo_app.php?hapus=1&key=<?php echo $key; ?>" onclick="return confirm('Apa anda yakin untuk menghapus todo ini?')">Hapus</a>
     </li>
-    <li>
-        <input type="checkbox" name="todo">
-        <label for="">Todo 2</label>
-        <a href="#">Hapus</a>
-    </li>
-    <li>
-        <input type="checkbox" name="todo">
-        <label for="">Todo 3</label>
-        <a href="#">Hapus</a>
-    </li>
-    <li>
-        <input type="checkbox" name="todo">
-        <label for="">Todo 4</label>
-        <a href="#">Hapus</a>
-    </li>
+    <?php endforeach; ?>
+  
 </ul>
